3ad8cf78f1ded33bda644b2ff6efd26dac65870b
James Fairbairn <jamesaf112@gmail.com>
2017-03-02 16:27:19 +0000

    Some tidying up

--- src/app/components/movie.component.ts
+++ src/app/components/movie.component.ts
@@ -14,37 +14,5 @@ export class MovieComponent {
     @Input() movie: Movie;
 
     constructor(private tmdbService: TMDBService) {
-        // this.movie.title = "Inception";
-        // this.movie.releaseDate = new Date("2010-07-14");
-        // // this.movie.posterUrl = "/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg";
-        // this.movie.viewings = [
-        //     {
-        //         id: 0,
-        //         date: new Date("2016-07-21"),
-        //         cinema: {
-        //             id: 0,
-        //             name: "Readings The Palms",
-        //             location: {
-        //                 latitude: -43.5073936,
-        //                 longitude: 172.6618957,
-        //             }
-        //         },
-        //         rewatch: false
-        //     },
-        //     {
-        //         id: 1,
-        //         date: new Date("2016-08-10"),
-        //         cinema: {
-        //             id: 0,
-        //             name: "Readings The Palms",
-        //             location: {
-        //                 latitude: -43.5073936,
-        //                 longitude: 172.6618957,
-        //             }
-        //         },
-        //         rewatch: true
-        //     },
-        // ];
-        // this.movie.overview = "Predominantly set during World War II, Steve Rogers is a sickly man from Brooklyn who's transformed into super-soldier Captain America to aid in the war effort. Rogers must stop the Red Skull – Adolf Hitler's ruthless head of weaponry, and the leader of an organization that intends to use a mysterious device of untold powers for world domination. Predominantly set during World War II, Steve Rogers is a sickly man from Brooklyn who's transformed into super-soldier Captain America to aid in the war effort. Rogers must stop the Red Skull – Adolf Hitler's ruthless head of weaponry, and the leader of an organization that intends to use a mysterious device of untold powers for world domination.";
     }
 }
